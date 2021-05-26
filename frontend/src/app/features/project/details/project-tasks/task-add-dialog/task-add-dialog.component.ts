84be2f8d57793608223f700ddbf075eca90c14f3
Lucas Eduardo <lucaseduardoormond@hotmail.com>
2021-05-26 12:21:17 -0300

    ajustes

--- frontend/src/app/features/project/details/project-tasks/task-add-dialog/task-add-dialog.component.ts
+++ frontend/src/app/features/project/details/project-tasks/task-add-dialog/task-add-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UsersService } from '@core/api/users.api';
+import { AuthService } from '@core/auth/auth.service';
 import { User } from '@core/entities/database-entities';
 import { TaskStatus } from '@core/entities/value-entities';
 import { ProjectFeatureService } from '@features/project/tools/project-feature.service';
@@ -20,6 +21,7 @@ export class TaskAddDialogComponent {
     private _dialogRef: MatDialogRef<TaskAddDialogComponent>,
     private _fb: FormBuilder,
     private _projectFeatureService: ProjectFeatureService,
+    private _authService: AuthService,
     private _usersService: UsersService,
   ) { }
 
@@ -27,11 +29,11 @@ export class TaskAddDialogComponent {
     title: ["", Validators.required],
     description: ["", Validators.required],
     status: [TaskStatus.InProgress],
-    userId: [null, Validators.required]
+    userId: ["", Validators.required]
   });
 
   get autocomplete() {
-    return this.form.get("autocomplete") as FormControl;
+    return this.form.get("userId") as FormControl;
   }
 
   autocomplete$ = fromForm(this.autocomplete).pipe(
@@ -39,6 +41,8 @@ export class TaskAddDialogComponent {
   );
 
   project$ = this._projectFeatureService.currentProject$;
+
+  loggedUser$ = this._authService.user$;
 
   users$ = this._usersService.getAll().pipe(
     switchMap(users => this.project$.pipe(
