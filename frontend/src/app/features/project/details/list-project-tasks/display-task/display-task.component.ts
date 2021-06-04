482d712847cea1fd7d0c60a3fac2c5b346fb79bc
Lucas Eduardo <lucaseduardoormond@hotmail.com>
2021-06-04 14:10:14 -0300

    evita epic ser propria epic

--- frontend/src/app/features/project/details/list-project-tasks/display-task/display-task.component.ts
+++ frontend/src/app/features/project/details/list-project-tasks/display-task/display-task.component.ts
@@ -71,7 +71,7 @@ export class DisplayTaskComponent implements AfterViewInit {
     map((project) => {
         if (!project) return [];
 
-        let tasks = project.tasks.filter(t => t.type.valueOf() === TaskType.Epic.valueOf());
+        let tasks = project.tasks.filter(t => t.type.valueOf() === TaskType.Epic.valueOf() && t.id !== this.task.id);
         return tasks;
     })
   );
